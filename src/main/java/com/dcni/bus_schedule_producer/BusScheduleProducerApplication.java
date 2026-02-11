0e4d1f550504fe3c82bd092312852245bf2977cb
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-02-10 23:47:11 -0300

    Archivos iniciales

---
+++ src/main/java/com/dcni/bus_schedule_producer/BusScheduleProducerApplication.java
@@ -0,0 +1,13 @@
+package com.dcni.bus_schedule_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusScheduleProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusScheduleProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcni/bus_schedule_producer/BusScheduleProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcni.bus_schedule_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusScheduleProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
